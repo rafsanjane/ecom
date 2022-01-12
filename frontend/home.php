0e629b9fbb13e5be2339ebae733bdcb2bd0cec8a
Md. Golam Rafsan Jane <rafsanhoby@gmail.com>
2022-01-12 10:35:17 +0600

    Frontend template mastering

---
+++ frontend/home.php
@@ -0,0 +1,14 @@
+<?php require_once 'partials/header.php'; ?>
+
+<main>
+
+    <?php require_once 'home/slider.php'; ?>
+    <?php require_once 'home/new_product.php'; ?>
+    <?php require_once 'home/gallery.php'; ?>
+    <?php require_once 'home/popular_items.php'; ?>
+    <?php require_once 'home/video_section.php'; ?>
+    <?php require_once 'home/more_info.php'; ?>
+
+</main>
+
+<?php require_once 'partials/footer.php'; ?>
---
+++ frontend/home/video_section.php
@@ -0,0 +1,54 @@
+    <!--? Video Area Start -->
+    <div class="video-area">
+        <div class="container-fluid">
+            <div class="row align-items-center">
+                <div class="col-lg-12">
+                    <div class="video-wrap">
+                        <div class="play-btn "><a class="popup-video" href="https://www.youtube.com/watch?v=KMc6DyEJp04"><i class="fas fa-play"></i></a></div>
+                    </div>
+                </div>
+            </div>
+            <!-- Arrow -->
+            <div class="thumb-content-box">
+                <div class="thumb-content">
+                    <h3>Next Video</h3>
+                    <a href="#"> <i class="flaticon-arrow"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Video Area End -->
+    <!--? Watch Choice  Start-->
+    <div class="watch-area section-padding30">
+        <div class="container">
+            <div class="row align-items-center justify-content-between padding-130">
+                <div class="col-lg-5 col-md-6">
+                    <div class="watch-details mb-40">
+                        <h2>Watch of Choice</h2>
+                        <p>Enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse.</p>
+                        <a href="shop.html" class="btn">Show Watches</a>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-10">
+                    <div class="choice-watch-img mb-40">
+                        <img src="frontend/assets/img/gallery/choce_watch1.png" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="row align-items-center justify-content-between">
+                <div class="col-lg-6 col-md-6 col-sm-10">
+                    <div class="choice-watch-img mb-40">
+                        <img src="frontend/assets/img/gallery/choce_watch2.png" alt="">
+                    </div>
+                </div>
+                <div class="col-lg-5 col-md-6">
+                    <div class="watch-details mb-40">
+                        <h2>Watch of Choice</h2>
+                        <p>Enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse.</p>
+                        <a href="shop.html" class="btn">Show Watches</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Watch Choice  End-->
