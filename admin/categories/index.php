d8185ce83b9ccac81465d8d0a405d94c309e7a99
rafsanjane <rafsanjanihoby@gmail.com>
2022-01-31 07:04:53 +0600

    Matching with main Project

--- admin/categories/index.php
+++ admin/categories/index.php
@@ -1,1 +1,32 @@
-ff
+<?php require_once __DIR__.'/../partials/header.php' ?>
+
+
+
+
+</div>
+</div>
+<div class="card">
+    <div class="card-header">
+        <a href="create.php">
+            <button class="btn btn-primary" type="submit"></button>
+        </a>
+    </div>
+    <div class="card-content">
+        <div class="card-body">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope='col'>#</th>
+                        <th scope='col'>CAtegory</th>
+                        <th scope='col'>Image</th>
+                        <th scope='col'>Action</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    </div>
+</div>
+
+
+
+<?php require_once __DIR__.'/../partials/footer.php' ?>
--- admin/dashboard/index.php
+++ admin/dashboard/index.php
@@ -1,13 +1,3 @@
-<?php require_once __DIR__.'/../partials/header.php' ?>
+<?php require_once __DIR__.'/../partials/header.php'; ?>
 
-
-<?php require_once __DIR__.'/../partials/sidebar.php' ?>
-
-
-        <div id="main">
-            
-        </div>
-
-
-
-<?php require_once __DIR__.'/../partials/footer.php' ?>
+<?php require_once __DIR__.'/../partials/footer.php'; ?>
